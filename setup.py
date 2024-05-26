35ba3c1184063462519de075145521039acd8d56
jiale <shanke607@qq.com>
2024-05-26 16:26:00 +0200

    v0.2

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="softagent",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your package needs here
+    ],
+    author="Xingyu-Lin et al.",
+    description="Agents used in SoftGym: Benchmarking Deep Reinforcement Learning for Deformable Object Manipulation",
+    url="https://github.com/Xingyu-Lin/softagent",
+)
